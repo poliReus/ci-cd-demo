ce9f569adb467739865d9813dc56ea510ed225b8
Fabio Rossi <fabio11.rossi@mail.polimi.it>
2025-02-05 17:33:39 +0100

    Initial commit for app and its tests

---
+++ app.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press ⌃F5 to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def add(a,b):
+    return a+b
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(add(5,4))
+
