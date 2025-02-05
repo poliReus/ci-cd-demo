8e89e3b03777e2838262012119ae23468bfad61c
Fabio Rossi <fabio11.rossi@mail.polimi.it>
2025-02-05 17:47:14 +0100

    fixes to test

--- test/test_app.py
+++ test/test_app.py
@@ -3,7 +3,7 @@ from app.app import add
 
 class TestApp(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(2, 3), 6)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
 
